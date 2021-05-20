bfead192e303529815bcefc26bb2d52e0672fe63
lisijie <414024639@qq.com>
2021-05-21 00:57:47 +0800

    同账号、同pc、同项目、不同目录中clone工程，分别push，能否检测出冲突，文件版本索引是否唯一可用测试。

--- src/main/java/com/lisijietech/learn/App.java
+++ src/main/java/com/lisijietech/learn/App.java
@@ -9,6 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        
+        //同账号、同pc、同项目、不同目录中clone工程，分别push，能否检测出冲突，文件版本索引是否唯一可用测试。
+        System.out.println( "测试" );
     }
 }
